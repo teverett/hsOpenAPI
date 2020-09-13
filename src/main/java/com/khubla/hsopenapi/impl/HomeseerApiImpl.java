a0a1f2d4ee66ccf0fb977d1fe348dbbb407bc661
Tom Everett <tom@khubla.com>
2020-09-13 17:18:06 -0600

    getsystems

--- src/main/java/com/khubla/hsopenapi/impl/HomeseerApiImpl.java
+++ src/main/java/com/khubla/hsopenapi/impl/HomeseerApiImpl.java
@@ -1,15 +1,65 @@
 package com.khubla.hsopenapi.impl;
 
+import java.io.*;
 import java.util.*;
 
+import javax.ws.rs.*;
+
+import org.modelmapper.*;
 import org.openapitools.api.*;
 import org.openapitools.model.System;
+import org.slf4j.*;
+
+import com.khubla.hsclient.*;
+import com.khubla.hsclient.domain.*;
+import com.khubla.hsopenapi.*;
 
 public class HomeseerApiImpl implements HomeseerApi {
+	/**
+	 * logger
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(HomeseerApiImpl.class);
+
+	/**
+	 * get logged in HSClient
+	 * 
+	 * @return an HSClient
+	 * @throws HSClientException
+	 * @throws IOException
+	 */
+	private HSClient getHSClient() throws HSClientException, IOException {
+		HSClient ret = new HSClientImpl();
+		ret.connect(Configuration.getInstance().getHsConfiguration());
+		return ret;
+	}
+
 	// /homeseer/getsystems
 	@Override
 	public List<System> getSystems() {
-		// TODO Auto-generated method stub
-		return null;
+		HSClient hsClient = null;
+		try {
+			final ModelMapper modelMapper = new ModelMapper();
+			hsClient = getHSClient();
+			List<HSSystem> lst = hsClient.getSystems();
+			if (null != lst) {
+				List<System> systems = new ArrayList<System>();
+				for (HSSystem hsSystem : lst) {
+					systems.add(modelMapper.map(hsSystem, System.class));
+				}
+				return systems;
+			} else {
+				throw new NotFoundException();
+			}
+		} catch (Exception e) {
+			throw new InternalServerErrorException(e);
+		} finally {
+			try {
+				if (null != hsClient) {
+					hsClient.close();
+				}
+			} catch (Exception e) {
+				logger.warn("Exception closing HSClient", e);
+			}
+		}
 	}
 }
